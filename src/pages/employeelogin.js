035cea65ae22f9949e18531da70a10386ea02228
raguram1224 <114975955+raguram1224@users.noreply.github.com>
2023-01-17 15:19:35 +0530

    Update employeelogin.js

--- src/pages/employeelogin.js
+++ src/pages/employeelogin.js
@@ -1,25 +1,24 @@
 import React from "react";
 import "./style.css"
-const employeelogin = () => {
-    <div>
-        <div class="content">
+const EmployeeLogin = () => {
+    // <div>
+    //     <div class="content">
 
-            <div class="text">Login Form</div>
+    //         <div class="text">Login Form</div>
 
-            <div class="field">
-                <span class="fas fa-user"></span>
-                <input id="empid" type="text" required />
-                <label>Email or Employee ID</label>
-            </div>
-            <div class="field">
-                <span class="fas fa-eye"></span>
-                <input id="otp" type="number" name="otp" pattern="[0-9]" title="OTP" />
-                <label>Enter Your OTP</label>
-            </div>
+    //         <div class="field">
+    //             <span class="fas fa-user"></span>
+    //             <input id="empid" type="text" required />
+    //             <label>Email or Employee ID</label>
+    //         </div>
+    //         <div class="field">
+    //             <span class="fas fa-eye"></span>
+    //             <input id="otp" type="number" name="otp" pattern="[0-9]" title="OTP" />
+    //             <label>Enter Your OTP</label>
+    //         </div>
 
-            <button>Sign in</button>
-            <p>Are u an admin?<span /><a href="admin.js">Click Here</a></p><span />
-        </div>
-    </div>
-}
-export default employeelogin;
+    //         <button>Sign in</button>
+    //         <p>Are u an admin?<span /><a href="Admin.js">Click Here</a></p><span />
+    //     </div>
+    // </div>
+    <h1>Hello Employee</h1>
