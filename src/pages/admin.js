a380d622229e3254d43fd541c59c4392240e958c
raguram1224 <114975955+raguram1224@users.noreply.github.com>
2023-01-17 15:18:53 +0530

    Update admin.js

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,25 +1,24 @@
 import React from "react";
 import "./style.css"
-const admin= () =>
-{
+const Admin = () => {
     <div>
-  <div class="text">Admin Login</div>
-       
-            <div class="field">
-                <span class="fas fa-user"></span>
-                <input type="text" required/>
-                <label>User name</label>
-            </div>
-            <div class="field">
-                <span class="fas fa-lock"></span>
-                <input type="password"/>
-                <label>Password</label>
-            </div>
-            <div class="forgot-pass"><a href="#">Forgot Password?</a></div>
-            <button>Sign in</button>
-            <p>Are u a Employee?<span/><a href="iemployeelogin.js">Click Here</a></p><span/>
+        <div class="text">Admin Login</div>
+
+        <div class="field">
+            <span class="fas fa-user"></span>
+            <input type="text" required />
+            <label>User name</label>
+        </div>
+        <div class="field">
+            <span class="fas fa-lock"></span>
+            <input type="password" />
+            <label>Password</label>
+        </div>
+        <div class="forgot-pass"><a href="#">Forgot Password?</a></div>
+        <button>Sign in</button>
+        <p>Are u a Employee?<span /><a href="EmployeeLogin.js">Click Here</a></p><span />
     </div>
-  
- 
+
+
 }
-export default admin;
+export default Admin;
