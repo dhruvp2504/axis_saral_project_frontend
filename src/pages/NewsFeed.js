6d93cc59ef044fab3a523f3027dedd3e3e0a1bcb
Harsh Nigam <nigamharshraj@gmail.com>
2023-01-31 10:22:55 +0530

    comment getting added

--- src/pages/NewsFeed.js
+++ src/pages/NewsFeed.js
@@ -8,7 +8,14 @@ import React, { useState, useEffect } from 'react';
 import { getHeadline } from './api';
 
 function NewsFeed() {
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
   const [headline, setHeadline] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setComments([...comments, newComment]);
+    setNewComment('');
+  };
 
   useEffect(() => {
     const fetchHeadline = async () => {
@@ -21,19 +28,31 @@ function NewsFeed() {
   return (
     <div className="card" style={{ width: "50%", margin: "10px auto 0 auto" }}>
       <h3>{headline}</h3>
-      <form>
-        <input type="text" placeholder="Name" />
-        <textarea placeholder="Add your comment"></textarea>
-        <button type="submit">Submit</button>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+        />
+        <button type="submit">Submit Comment</button>
       </form>
     </div>
   );
 }
 
+
+
+
+
+
+
+
+
+
+
 export default NewsFeed;
-
-
-
-
-
-
