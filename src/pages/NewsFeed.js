e9562db8196720343f053cbddd061e1b2fb9b77a
Harsh Nigam <nigamharshraj@gmail.com>
2023-01-31 09:24:11 +0530

    newsfeed partially done

--- src/pages/NewsFeed.js
+++ src/pages/NewsFeed.js
@@ -1,3 +1,39 @@
-export default function NewsFeed() {
-    return <h1>newfeed</h1>
+/*export default function NewsFeed() {
+    return <h1>news</h1>
+}*/
+
+
+
+import React, { useState, useEffect } from 'react';
+import { getHeadline } from './api';
+
+function NewsFeed() {
+  const [headline, setHeadline] = useState('');
+
+  useEffect(() => {
+    const fetchHeadline = async () => {
+      const fetchedHeadline = await getHeadline();
+      setHeadline(fetchedHeadline);
+    };
+    fetchHeadline();
+  }, []);
+
+  return (
+    <div className="card" style={{ width: "50%", margin: "10px auto 0 auto" }}>
+      <h3>{headline}</h3>
+      <form>
+        <input type="text" placeholder="Name" />
+        <textarea placeholder="Add your comment"></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
+
+export default NewsFeed;
+
+
+
+
+
+
